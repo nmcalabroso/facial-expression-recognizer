fd742884c3773ce98ff01426567d0281160e5607
gardevior411 <earle.bunao@gmail.com>
2014-03-22 00:49:30 +0800

    Feed forward

--- ann.py
+++ ann.py
@@ -14,11 +14,11 @@ class ANN():
 
 		num_layers = hidden_n+2
 
-		sizes = [input_n+1 if i is 0 else output_n if i is num_layers-1 else num_nodes if i is num_layers-2 else num_nodes+1 for i in range(num_layers)]
+		self.sizes = [input_n+1 if i is 0 else output_n if i is num_layers-1 else num_nodes if i is num_layers-2 else num_nodes+1 for i in range(num_layers)]
 
 		rand = lambda x: 0.01 if x is 0 else x
 		
-		self.weight = [ [ [rand(random()) for k in range(sizes[i+1])] for j in range(sizes[i])] for i in range(num_layers-1)]
+		self.weight = [ [ [rand(random()) for k in range(self.sizes[i+1])] for j in range(self.sizes[i])] for i in range(num_layers-1)]
 
 		self.output = [0 for i in range(output_n)]
 
@@ -33,10 +33,10 @@ class ANN():
 			self.hidden[row] = 
 
 	def feed_forward(self,layer):
-		if layer == 0: #input layer and 1st hidden layer
-			sum(map((lambda e: e[0]*e[1]),zip(self.weight[layer],self.input)))
+		if layer == 1: #input layer and 1st hidden layer
+			nodes_value = self.input
+		else:
+			nodes_value = self.hidden[layer-2]
 
-#   input layer
-#    nodes
-#      
-#[ [ [],...,],[],[]]
+		self.hidden[layer] = [sum(map((lambda e: e[0]*e[1]),zip([i[j] for i in self.weight[layer-1]],nodes_value))) for j in range(self.sizes[layer])]
+
